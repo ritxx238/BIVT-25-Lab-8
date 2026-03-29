b72cd3998eff567d34cadabb72805b0ff4832d5f
ritxx238 <zarita4815@gmail.com>
2026-03-29 11:18:22 +0300

    Update Task1.cs

--- Lab8/Green/Task1.cs
+++ Lab8/Green/Task1.cs
@@ -1,214 +1,120 @@
-using System.Reflection;
-using System.Text.Json;
-
-namespace Lab8Test.Green
+namespace Lab8.Green
 {
-    [TestClass]
-    public sealed class Task1
+    public class Task1
     {
-        record InputRow(string Surname, string Group, string Trainer, double Result);
-        record OutputRow(string Surname, string Group, string Trainer, double Result, bool HasPassed);
-
-        private InputRow[] _input;
-        private OutputRow[] _output;
-        private Lab8.Green.Task1.Participant[] _participants;
-
-        [TestInitialize]
-        public void LoadData()
+        public abstract class Participant
         {
-            var folder = Directory.GetParent(Directory.GetCurrentDirectory())
-                .Parent.Parent.Parent.FullName;
-            folder = Path.Combine(folder, "Lab8Test", "Green");
+            private string _surname;
+            private string _group;
+            private string _trainer;
+            private double _result;
 
-            var input = JsonSerializer.Deserialize<JsonElement>(
-                File.ReadAllText(Path.Combine(folder, "input.json")))!;
-            var output = JsonSerializer.Deserialize<JsonElement>(
-                File.ReadAllText(Path.Combine(folder, "output.json")))!;
+            protected double _standard;
 
-            _input = input.GetProperty("Task1").Deserialize<InputRow[]>()!;
-            _output = output.GetProperty("Task1").Deserialize<OutputRow[]>()!;
+            private static int _passed;
 
-            _participants = new Lab8.Green.Task1.Participant[_input.Length];
-        }
+            public string Surname => _surname;
+            public string Group => _group;
+            public string Trainer => _trainer;
+            public double Result => _result;
+            public static int PassedTheStandard => _passed;
 
-        [TestMethod]
-        public void Test_00_OOP()
-        {
-            var baseType = typeof(Lab8.Green.Task1.Participant);
-            var t100 = typeof(Lab8.Green.Task1.Participant100M);
-            var t500 = typeof(Lab8.Green.Task1.Participant500M);
-
-            Assert.IsTrue(baseType.IsClass);
-            Assert.IsTrue(baseType.IsAbstract);
-
-            Assert.IsTrue(t100.IsSubclassOf(baseType));
-            Assert.IsTrue(t500.IsSubclassOf(baseType));
-
-            Assert.AreEqual(0, baseType.GetFields(BindingFlags.Public | BindingFlags.Instance).Length);
-            Assert.AreEqual(0, t100.GetFields(BindingFlags.Public | BindingFlags.Instance).Length);
-            Assert.AreEqual(0, t500.GetFields(BindingFlags.Public | BindingFlags.Instance).Length);
-
-            Assert.IsTrue(baseType.GetProperty("Surname")?.CanRead ?? false);
-            Assert.IsTrue(baseType.GetProperty("Group")?.CanRead ?? false);
-            Assert.IsTrue(baseType.GetProperty("Trainer")?.CanRead ?? false);
-            Assert.IsTrue(baseType.GetProperty("Result")?.CanRead ?? false);
-            Assert.IsTrue(baseType.GetProperty("HasPassed")?.CanRead ?? false);
-
-            Assert.IsFalse(baseType.GetProperty("Surname")?.CanWrite ?? true);
-            Assert.IsFalse(baseType.GetProperty("Group")?.CanWrite ?? true);
-            Assert.IsFalse(baseType.GetProperty("Trainer")?.CanWrite ?? true);
-            Assert.IsFalse(baseType.GetProperty("Result")?.CanWrite ?? true);
-            Assert.IsFalse(baseType.GetProperty("HasPassed")?.CanWrite ?? true);
-
-            Assert.IsNotNull(baseType.GetConstructor(
-                BindingFlags.Instance | BindingFlags.Public,
-                null,
-                new[] { typeof(string), typeof(string), typeof(string) },
-                null));
-
-            Assert.IsNotNull(t100.GetConstructor(
-                BindingFlags.Instance | BindingFlags.Public,
-                null,
-                new[] { typeof(string), typeof(string), typeof(string) },
-                null));
-
-            Assert.IsNotNull(t500.GetConstructor(
-                BindingFlags.Instance | BindingFlags.Public,
-                null,
-                new[] { typeof(string), typeof(string), typeof(string) },
-                null));
-
-            Assert.IsNotNull(baseType.GetMethod("Run", BindingFlags.Instance | BindingFlags.Public));
-            Assert.IsNotNull(baseType.GetMethod("GetTrainerParticipants", BindingFlags.Static | BindingFlags.Public));
-
-            Assert.AreEqual(0, baseType.GetFields().Count(f => f.IsPublic));
-            Assert.AreEqual(baseType.GetProperties().Count(f => f.PropertyType.IsPublic), 6);
-            Assert.AreEqual(baseType.GetConstructors().Count(f => f.IsPublic), 1);
-            Assert.AreEqual(baseType.GetMethods(BindingFlags.Instance | BindingFlags.Public).Length, 11);
-
-            Assert.IsNotNull(t100.GetMethod("Run", BindingFlags.Instance | BindingFlags.Public));
-            Assert.IsNull(t100.GetMethod("GetTrainerParticipants", BindingFlags.Static | BindingFlags.Public));
-
-            Assert.AreEqual(0, t100.GetFields().Count(f => f.IsPublic));
-            Assert.AreEqual(t100.GetProperties().Count(f => f.PropertyType.IsPublic), 5);
-            Assert.AreEqual(t100.GetConstructors().Count(f => f.IsPublic), 1);
-            Assert.AreEqual(t100.GetMethods(BindingFlags.Instance | BindingFlags.Public).Length, 11);
-
-            Assert.IsNotNull(t500.GetMethod("Run", BindingFlags.Instance | BindingFlags.Public));
-            Assert.IsNull(t500.GetMethod("GetTrainerParticipants", BindingFlags.Static | BindingFlags.Public));
-
-            Assert.AreEqual(0, t500.GetFields().Count(f => f.IsPublic));
-            Assert.AreEqual(t500.GetProperties().Count(f => f.PropertyType.IsPublic), 5);
-            Assert.AreEqual(t500.GetConstructors().Count(f => f.IsPublic), 1);
-            Assert.AreEqual(t500.GetMethods(BindingFlags.Instance | BindingFlags.Public).Length, 11);
-        }
-        [TestMethod]
-        public void Test_01_Create()
-        {
-            Init();
-        }
-
-        [TestMethod]
-        public void Test_02_Run()
-        {
-            Init();
-            Run();
-            Check(runExpected: true);
-        }
-
-        [TestMethod]
-        public void Test_03_PassedCount()
-        {
-            Init();
-            Run();
-
-            int expected = _output.Count(o => o.HasPassed);
-            Assert.AreEqual(expected, Lab8.Green.Task1.Participant.PassedTheStandard);
-        }
-
-        [TestMethod]
-        public void Test_04_GetTrainerParticipants()
-        {
-            Init();
-            Run();
-
-            var result = Lab8.Green.Task1.Participant.GetTrainerParticipants(
-                _participants,
-                typeof(Lab8.Green.Task1.Participant500M),
-                "Свиридов");
-
-            Assert.IsTrue(result.All(p =>
-                p.Trainer == "Свиридов" &&
-                p is Lab8.Green.Task1.Participant500M));
-        }
-        private void ResetAllParticipantStatics()
-        {
-            var baseType = typeof(Lab8.Green.Task1.Participant);
-
-            var allTypes = baseType.Assembly
-                .GetTypes()
-                .Where(t => baseType.IsAssignableFrom(t));
-
-            foreach (var type in allTypes)
+            public bool HasPassed
             {
-                var staticFields = type.GetFields(
-                    BindingFlags.Static |
-                    BindingFlags.Public |
-                    BindingFlags.NonPublic);
-
-                foreach (var field in staticFields)
+                get
                 {
-                    if (field.FieldType == typeof(int))
-                        field.SetValue(null, 0);
-                    else if (field.FieldType == typeof(double))
-                        field.SetValue(null, 0.0);
-                    else if (field.FieldType == typeof(bool))
-                        field.SetValue(null, false);
+                    if (_result == 0)
+                    {
+                        return false;
+                    }
                     else
-                        field.SetValue(null, null);
+                    {
+                        if (_result <= _standard)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
                 }
             }
-        }
-        private void Init()
-        {
-            ResetAllParticipantStatics();
-            for (int i = 0; i < _input.Length; i++)
+
+            public Participant(string surname, string group, string trainer)
             {
-                _participants[i] =
-                    i % 2 == 0
-                        ? new Lab8.Green.Task1.Participant100M(
-                            _input[i].Surname,
-                            _input[i].Group,
-                            _input[i].Trainer)
-                        : new Lab8.Green.Task1.Participant500M(
-                            _input[i].Surname,
-                            _input[i].Group,
-                            _input[i].Trainer);
+                _surname = surname;
+                _group = group;
+                _trainer = trainer;
+                _result = 0;
             }
-        }
 
-        private void Run()
-        {
-            for (int i = 0; i < _input.Length; i++)
+            static Participant()
             {
-                _participants[i].Run(_input[i].Result);
-                _participants[i].Run(-1);
+                _passed = 0;
             }
-        }
 
-        private void Check(bool runExpected)
-        {
-            for (int i = 0; i < _participants.Length; i++)
+            public void Run(double result)
             {
-                Assert.AreEqual(_output[i].Surname, _participants[i].Surname);
-                Assert.AreEqual(_output[i].Group, _participants[i].Group);
-                Assert.AreEqual(_output[i].Trainer, _participants[i].Trainer);
-
-                if (runExpected)
+                if (_result == 0)
                 {
-                    Assert.AreEqual(_output[i].Result, _participants[i].Result, 0.0001);
-                    Assert.AreEqual(_output[i].HasPassed, _participants[i].HasPassed);
+                    _result = result;
+
+                    if (result <= _standard)
+                    {
+                        _passed++;
+                    }
                 }
+            }
+
+            public static Participant[] GetTrainerParticipants(Participant[] participants, Type participantType,
+                string trainer)
+            {
+                int count = 0;
+
+                for (int i = 0; i < participants.Length; i++)
+                {
+                    if (participants[i].GetType() == participantType && participants[i].Trainer == trainer)
+                    {
+                        count++;
+                    }
+                }
+
+                Participant[] result = new Participant[count];
+
+                int index = 0;
+
+                for (int i = 0; i < participants.Length; i++)
+                {
+                    if (participants[i].GetType() == participantType && participants[i].Trainer == trainer)
+                    {
+                        result[index] = participants[i];
+                        index++;
+                    }
+                }
+
+                return result;
+            }
+
+            public void Print()
+            {
+                return;
+            }
+        }
+
+        public class Participant100M : Participant
+        {
+            public Participant100M(string surname, string group, string trainer) : base(surname, group, trainer)
+            {
+                _standard = 12;
+            }
+        }
+
+        public class Participant500M : Participant
+        {
+            public Participant500M(string surname, string group, string trainer) : base(surname, group, trainer)
+            {
+                _standard = 90;
             }
         }
     }
